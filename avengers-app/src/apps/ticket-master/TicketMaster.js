a2cfe6a204f3eb12c579c758be46fdd5ac0987ce
Shannon D <sdillman@users.noreply.github.com>
2021-07-04 17:42:09 -0400

    least broken version of Ticketmaster so far. Still need logic written to parse the object returned from TM, when we get it. Sometimes we see CORS errors. At the time of this commit, we do not. We also need to update the DOM with whatever we come up with for events

--- avengers-app/src/apps/ticket-master/TicketMaster.js
+++ avengers-app/src/apps/ticket-master/TicketMaster.js
@@ -1,23 +1,76 @@
-
 const TicketMaster = (props) => {
+    // import { useEffect } from 'react';
+
+    // return (
+    //     <div>
+    //         <br />
+    //         <h3>
+    //         OpenWeather API
+    //         </h3>
+    //         open weather content
+    //     </div>
+
+    // );
 
     
-    return (
+    const {lat, long} = props;
+
+    console.log(`Lat: ${lat}, Lon: ${long}`);
+
+
+    const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=etyd9ynkKnLqJWFl0KV66dolqNYGtCIK&latlong=${lat},${long}&radius=25&unit=miles&source=%20universe&locale=*`;
+
+    // let localEvents = JSON.parse(url);
+    // console.log(localEvents);
+
+    const getLocalEvents = async () => {
+        let display=undefined;
+        try {
+            const res = await fetch(url);
+            const jsonData = res.json();
+            // console.log(`***** Data: ${JSON.stringify(jsonData, null,4)}, ${JSON.stringify(jsonData._embedded.events[0],null,4)}`);
+            console.log(`***** Drilled-in Data: ${JSON.stringify(jsonData._embedded.events[0].name,null,5)}`);
+            display = JSON.stringify(jsonData._embedded.events[0].name);
+            return display;
+        } catch(err) {
+            console.error(`Error: ${err},`);
+            return
+        }
+
+        return "RETURN DISPLAY HERE " + display;
+    };
+
+    // let display = getLocalEvents();
+    // useEffect(() => {
+    //     document.getElementById('testmeh').innerText = 'test meee';
+    // });
+
+    // document.getElementById('testmeh').innerText = 'test meee';
+
+    // const displayEvent = getLocalEvents();
+
+return lat && long ? 
+        (
         <div>
             <br />
-            <h3>
-            TicketMaster API
-            </h3>
-            <p>
-                ticket master content
-                {/* {props.lat}
-                <br />
-                {props.long} */}
-            </p>
+            <h3>Events within about 25 miles of you</h3>
+            <div>
+                <p>if we get lat lon, list any returns here</p>
+                <p id='testmeh'>display</p>
+                {/* list them out here */}
+            </div>
+        </div> 
+        )
+    :
+        (
+            // if we don't get lat lon, serve this instead
+        <div>
+            <br />
+            <h3>Events within about 25 minles of you</h3>
+            <p>Unfortunately, we can't determine your location. In a broad sense, then, all events on Earth could be available to you, though not all will be convenient.</p>
         </div>
+        );
 
-    );
 };
-
 
 export default TicketMaster;
