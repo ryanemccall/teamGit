98f3ec4916ff8de292b653bf80c800f76c917f93
Ryan Modrall <ryanemccall20@gmail.com>
2021-07-05 16:10:30 -0400

    Working Open Weather API awaiting KEY to to be unblocked

--- avengers-app/src/apps/open-weather/OpenWeather.js
+++ avengers-app/src/apps/open-weather/OpenWeather.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
+const baseURL = 'https://api.openweathermap.org/data/2.5/onecall'
 const key = '025f21b62fdecae856acb53863070e47';
 
 const OpenWeather = (props) => {
@@ -8,20 +8,28 @@ const OpenWeather = (props) => {
 const [search, setSearch] = useState('');
 const [results, setResults] = useState([]);
     const fetchResults = () => {
-        let url = `${baseURL}?lat=${lat}&lon=${long}&appid=${key}`
+        let url = `${baseURL}?lat=${lat}&lon=${long}&exclude=minutely&appid=${key}`
 
         fetch(url)
         .then(res => res.json())
-        .then(data => setResults(data.response.docs))
+        .then(data => console.log(data))
         .catch(err => console.log(err));
+        
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetchResults();
     }
+    
     return (
         <div>
-            <br />
             <h3>
             OpenWeather API
             </h3>
             open weather content
+            <div>
+                <button onClick={(event) => handleSubmit(event)}>Click Here for Current Weather in your Area!</button>
+            </div>
         </div>
 
     );
