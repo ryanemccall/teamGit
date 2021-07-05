178d634b12c78f6e35320a4f22f0ba6da99f1049
Shannon D <sdillman@users.noreply.github.com>
2021-07-05 01:42:04 -0400

    current best effort. Issues and todos are numerous and mostly laid out in comments and the code. Display varies based on whether or not we get location data. But still unable to list events to the page.

--- avengers-app/src/apps/ticket-master/TicketMaster.js
+++ avengers-app/src/apps/ticket-master/TicketMaster.js
@@ -1,6 +1,8 @@
+import { useEffect, useState, useRef } from 'react';
 const TicketMaster = (props) => {
-    // import { useEffect } from 'react';
-
+    
+    let [eventName, setEventName] = useState("");
+    let [eventURL, setEventURL] = useState("");
     // return (
     //     <div>
     //         <br />
@@ -14,68 +16,90 @@ const TicketMaster = (props) => {
 
     
     const {lat, long} = props;
+    let url = `https://app.ticketmaster.com/discovery/v2/events?apikey=etyd9ynkKnLqJWFl0KV66dolqNYGtCIK&latlong=${lat},${long}&radius=25&unit=miles&source=%20universe&locale=*`;
 
     console.log(`top of TicketMaster - Lat: ${lat}, Lon: ${long}`);
+
+    useRef(() => {
+        if (lat && long) {
+            url = `https://app.ticketmaster.com/discovery/v2/events?apikey=etyd9ynkKnLqJWFl0KV66dolqNYGtCIK&latlong=${lat},${long}&radius=25&unit=miles&source=%20universe&locale=*`;
+        }
+    });
     
 
-    if (lat && long) {
-    const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=etyd9ynkKnLqJWFl0KV66dolqNYGtCIK&latlong=${lat},${long}&radius=25&unit=miles&source=%20universe&locale=*`;
 
-    console.log(url);
+    console.log(`URL: ${url}`);
 
-    // let localEvents = JSON.parse(url);
-    // console.log(localEvents );
 
     const getLocalEvents = async () => {
-        try {
+        // This code only deals with one event - so so so brittle. But right now I would be happy to get ANY event onto the page, and not just console.log. Still, the giant TODO is to have it iterate through a collection of events, if any, and display, or fail gracefully when no events
             const res = await fetch(url);
-            const jsonData = res.json();
-            console.log(`***** Data: ${JSON.stringify(jsonData, null,4)}, ${JSON.stringify(jsonData._embedded.events[0].url,null,4)}`);
-            console.log(`***** Drilled-in Data: ${JSON.stringify(jsonData._embedded.events[0].name,null,5)}`);
-            let display = JSON.stringify(jsonData._embedded.events[0].name);
-            return display;
-        } catch(err) {
-            console.error(`Error: ${err},`);
-            return
-        }
+            const jsonData = await res.json();
 
-        // return "RETURN DISPLAY HERE " + display;
+            console.log(`***** EVENT URL Data: ${JSON.stringify(jsonData._embedded.events[0].url,null,4)}`);
+
+            console.log(`***** EVENT NAME Data: ${JSON.stringify(jsonData._embedded.events[0].name,null,5)}`);
+
+            setEventName(() => {
+                eventName = jsonData._embedded.events[0].name;
+            })
+            setEventURL(() => {
+                eventURL = jsonData._embedded.events[0].url;
+            })
+
+            console.log(`eventNAME: ${eventName} \n eventURL: ${eventURL}`);
+            return;
     };
 
+
+if (lat && long) {
     getLocalEvents();
-}
-
-
-    // let display = getLocalEvents();
-    // useEffect(() => {
-    //     document.getElementById('testmeh').innerText = 'test meee';
-    // });
-
-    // document.getElementById('testmeh').innerText = 'test meee';
-
-    // const displayEvent = getLocalEvents();
-
-return lat && long ? 
+    // document.getElementById('testmeh').innerText=eventName;
+    console.log(`last go at eventName ${eventName}`);
+    return (
         (
-        <div>
-            <br />
-            <h3>Events within about 25 miles of you</h3>
             <div>
-                <p>if we get lat lon, list any returns here</p>
-                <p id='testmeh'>display</p>
-                {/* list them out here */}
+                <br />
+                <h3>Events within about 25 miles of you</h3>
+                <div>
+                    <p>if we get lat lon, list any returns here</p>
+                    <p id='testmeh'>display</p>
+                    <p>{eventName}</p>
+                </div>
             </div>
-        </div> 
-        )
-    :
-        (
+            )
+    )
+} else {
+    return (
             // if we don't get lat lon, serve this instead
-        <div>
+            <div>
             <br />
             <h3>Events within about 25 miles of you</h3>
             <p>Unfortunately, we can't determine your location. In a broad sense, then, all events on Earth could be available to you, though not all will be convenient.</p>
         </div>
-        );
+    )
+}
+// return lat && long ? 
+//         (
+//         <div>
+//             <br />
+//             <h3>Events within about 25 miles of you</h3>
+//             <div>
+//                 <p>if we get lat lon, list any returns here</p>
+//                 <p id='testmeh'>display</p>
+//                 <p>{eventName}</p>
+//             </div>
+//         </div>
+//         )
+//     :
+//         (
+//             // if we don't get lat lon, serve this instead
+//         <div>
+//             <br />
+//             <h3>Events within about 25 miles of you</h3>
+//             <p>Unfortunately, we can't determine your location. In a broad sense, then, all events on Earth could be available to you, though not all will be convenient.</p>
+//         </div>
+//         );
 
 };
 
