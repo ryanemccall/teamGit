0fc27b792e06c4149a2a28b6c6ffddb448571f94
Ryan Modrall <ryanemccall20@gmail.com>
2021-07-06 16:35:59 -0400

    Last one

--- avengers-app/src/apps/ticket-master/TicketMaster.js
+++ avengers-app/src/apps/ticket-master/TicketMaster.js
@@ -1,64 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Button, Table, Row, Col } from 'reactstrap';
-import { useState } from "react";
-import TMEventResults from './TMEventResults'
 
-const TicketMaster = (props) => {
-
-    //declare URL & key
-    const baseUrl = "http://apiurl";
-    const apiKey = "haioasdfkmlkjicun"
-    //declare props here again to be called as lat and long
-    let lat = { props.lat };
-    let long = { props.long };
-    
-    const [eventResults, setEventResults] = useState([]);
-    
-    //set method to fetch results
-    //pass props inside 
-    const getEvents = () => {
-        event.preventDefault();
-        //set fetch url
-        let url = `${baseURL}?api-key=${apiKey}&lat=${lat}&long=${long}`
-        //do fetch
-        fetch(url)
-            //jsonify it 
-            .then(res => res.json())
-            //name object data so we can refer to it 
-            //then store it in the results var by calling on useState
-            .then(data => setEventResults(data.walkthruobj.totheevents))
-        console.log(data)
-            //display errors 
-            .catch(err => console.log(err.message))
-    };
-    
-    return (
-        <div className="mainDiv">
-            <br />
-            <h3>
-            TicketMaster API
-            </h3>
-            {/* <p>
-             {props.lat}
-                <br />
-                {props.long} 
-            </p> */}
-            {/* user location already retrieved
-            once click it fires fetch */}
-            <Button
-                onClick={getEvents}>
-                Get Events Near Me
-            </Button>
-{/*             
-            call on child component to display results */}
-            <TMEventResults
-                //pass event object/var into it
-                eventResults={eventResults}
-            />
-        
-        </div>
-=======
 import { useEffect, useState } from 'react';
 const TicketMaster = (props) => {
 
@@ -66,7 +6,6 @@ const TicketMaster = (props) => {
     const {lat, long} = props;
     
     let url = `https://app.ticketmaster.com/discovery/v2/events?apikey=etyd9ynkKnLqJWFl0KV66dolqNYGtCIK&latlong=${lat},${long}&radius=25&unit=miles&sort=random&locale=*`;
->>>>>>> 70935e9b836a8f461557ec1b260f020222e0f1b7
 
 
     useEffect(() => {
